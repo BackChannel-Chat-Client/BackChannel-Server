cfdc4d95b8960fb38266a10e7cccab65c72d71a9
hypervis0r <hyp3rvisor@protonmail.com>
2021-06-24 09:48:18 -0600

    Implement posix pthread

--- src/threads.c
+++ src/threads.c
@@ -3,13 +3,13 @@
 BC_STATUS
 Bc_Platform_StartThread(BC_THREAD_PROC thread_proc, void* parameter, BC_THREAD* thread_id)
 {
-	HANDLE thread_handle = NULL;
+	BC_THREAD thread_handle = NULL;
 
 	if (!thread_proc || !thread_id)
 		return BC_INVALID_PARAM;
 
 #ifdef _WIN32
-	thread_handle = CreateThread(
+    thread_handle = CreateThread(
 		NULL,
 		0,
 		(LPTHREAD_START_ROUTINE)thread_proc,
@@ -21,8 +21,17 @@ Bc_Platform_StartThread(BC_THREAD_PROC thread_proc, void* parameter, BC_THREAD* 
 
 	*thread_id = thread_handle;
 
-	return BC_SUCCESS;
 #else
-	return BC_UNIMPLEMENTED;
+    int result = pthread_create(
+            thread_handle,
+            NULL,
+            thread_proc,
+            parameter);
+    if (result)
+        BcFatalError("Failed to create connection thread");
+
+    *thread_id = thread_handle;
 #endif
+
+	return BC_SUCCESS;
 }
